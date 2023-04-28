3ac372590ad094aa39c0b28481216ec233b56f0a
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-28 20:41:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ drawCircle="";
 drawRect="";
 SpeechRecognition = window.webkitSpeechRecognition;
 recognition = new SpeechRecognition();
+function preload(){
+carro= loadImage("
+}
 function start(){
     document.getElementById("status").innerHTML = "O sistema está ouvindo. Por favor, fale.";
     recognition.start();
@@ -12,13 +15,13 @@ recognition.onresult = function(event){
     console.log(event);
     content = event.results[0][0].transcript;
     document.getElementById("status").innerHTML = "A sua fala foi reconhecida como: "+content;
-    if(content =="círculo"){
+    if(content =="carro")
         x = Math.floor(Math.random()* 900);
         y = Math.floor(Math.random()* 600);
         document.getElementById("status").innerHTML = "Desenhando um círculo.";
         drawCircle = "set";
     }
-    if(content =="retângulo"){
+    if(content =="pessoa")
         x = Math.floor(Math.random()* 900);
         y = Math.floor(Math.random()* 600);
         document.getElementById("status").innerHTML = "Desenhando um retângulo.";
@@ -31,7 +34,7 @@ function setup(){
 function draw(){
     if(drawCircle == "set"){
         radius = Math.floor(Math.random()* 100);
-        circle(x, y, radius);
+        image()
         document.getElementById("status").innerHTML= "círculo desenhado. ";
         drawCircle = ""
     }
