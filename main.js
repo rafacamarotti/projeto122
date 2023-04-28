087dc7bafd64d6d3065fdf7e7b7cd3c03e2e4ed7
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-28 20:54:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,13 +16,13 @@ recognition.onresult = function(event){
     console.log(event);
     content = event.results[0][0].transcript;
     document.getElementById("status").innerHTML = "A sua fala foi reconhecida como: "+content;
-    if(content =="carro")
+    if(content =="carro"){
         x = Math.floor(Math.random()* 900);
         y = Math.floor(Math.random()* 600);
         document.getElementById("status").innerHTML = "Desenhando um círculo.";
         drawCircle = "set";
     }
-    if(content =="pessoa")
+    if(content =="pessoa"){
         x = Math.floor(Math.random()* 900);
         y = Math.floor(Math.random()* 600);
         document.getElementById("status").innerHTML = "Desenhando um retângulo.";
